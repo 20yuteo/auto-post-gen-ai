dbff9a3b3081b59d3ff15851fdc332d2093ae388
20yuteo <20yuteo@gmail.com>
2025-03-19 18:20:04 +0900

    navbar作った

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,47 +11,40 @@ import { ColorModeButton } from "@/components/ui/color-mode";
 
 export default async function Home() {
   return (
-    <Box p={8} maxW="800px" mx="auto">
-      <Flex direction="column" align="center" justify="center">
-        <Image src="/next.svg" alt="Next.js logo" boxSize="180px" />
-        <ColorModeButton />
-        <List.Root>
-          <ListItem>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </ListItem>
-          <ListItem>Save and see your changes instantly.</ListItem>
-        </List.Root>
-        <Flex gap={4} mt={6}>
-          <Button as={Link} colorScheme="blue" rel="noopener noreferrer">
-            Deploy now
-          </Button>
-          <Button as={Link} rel="noopener noreferrer" variant="outline">
-            Read our docs
-          </Button>
+    <Box maxW="100vw" mx="auto" maxH="100vh">
+      <Flex direction="row" align="center" justify="space-between" maxW="100vw">
+        <Box
+          bg="dark"
+          p={4}
+          borderRight={"1px solid #343536"}
+          w="365px"
+          h="100vh"
+        >
+          <Flex direction="column" justify="space-between" align="center">
+            <Link href="/">Home</Link>
+            <Button width="100%" rounded={32}>
+              SUBMIT
+            </Button>
+          </Flex>
+        </Box>
+        <Flex direction="column" align="center" justify="center" w="100%">
+          <Image src="/next.svg" alt="Next.js logo" boxSize="180px" />
+          <ColorModeButton />
+          <List.Root>
+            <ListItem>
+              Get started by editing <code>src/app/page.tsx</code>.
+            </ListItem>
+            <ListItem>Save and see your changes instantly.</ListItem>
+          </List.Root>
+          <Flex gap={4} mt={6}>
+            <Button as={Link} colorScheme="blue" rel="noopener noreferrer">
+              Deploy now
+            </Button>
+            <Button as={Link} rel="noopener noreferrer" variant="outline">
+              Read our docs
+            </Button>
+          </Flex>
         </Flex>
-      </Flex>
-      <Flex as="footer" mt={10} justify="space-around">
-        <Link
-          href="https://nextjs.org/learn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </Link>
-        <Link
-          href="https://vercel.com/templates?framework=next.js"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Examples
-        </Link>
-        <Link
-          href="https://nextjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to nextjs.org →
-        </Link>
       </Flex>
     </Box>
   );
