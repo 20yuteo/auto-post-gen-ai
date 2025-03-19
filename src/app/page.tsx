65dfe23119c384248ac262feee6fa2fb3ae4d6ff
20yuteo <20yuteo@gmail.com>
2025-03-19 17:50:40 +0900

    chakra ui 導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,134 +1,56 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { RepositoryProvider } from "@/app/adapter/repositories/provider";
+import {
+  Box,
+  Flex,
+  Button,
+  Link,
+  ListItem,
+  Image,
+  List,
+} from "@chakra-ui/react";
 
 export default async function Home() {
-  const slackRepository = new RepositoryProvider().slack;
-  try {
-    const indieHackersRepository = new RepositoryProvider().indieHackers;
-    const llmRepository = new RepositoryProvider().llm;
-    const tweetRepository = new RepositoryProvider().tweet;
-
-    const res = await indieHackersRepository.getPosts();
-
-    if (!res) {
-      throw new Error("Failed to fetch posts");
-    }
-
-    const prompt = `
-      あなたは、xのポストを投稿するプロのSNSマーケターです。
-    
-      1. xのポストとして投稿するために、タイトルもしくはtextを日本語に翻訳してください
-       - title: ${res?.title}
-       - text: ${res?.text}
-      2. ポストの中にハッシュダグを入れて下さい。
-      3. ポストの中にURLが存在すれば以下のURLを入れて下さい。
-        - ${res?.url}
-      6. 出力形式はplane textにしてください。
-      7. ポストは140文字以内です。
-      `;
-
-    const content = await llmRepository.generateContent(prompt);
-
-    await tweetRepository.postTweet(content);
-
-    await slackRepository.postMessage(`Success: ${content}`);
-
-    console.log("Success");
-  } catch (error) {
-    await slackRepository.postMessage(`Error: ${error}`);
-
-    console.log("Error");
-  }
-
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
+    <Box p={8} maxW="800px" mx="auto">
+      <Flex direction="column" align="center" justify="center">
+        <Image src="/next.svg" alt="Next.js logo" boxSize="180px" />
+        <List.Root>
+          <ListItem>
             Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
+          </ListItem>
+          <ListItem>Save and see your changes instantly.</ListItem>
+        </List.Root>
+        <Flex gap={4} mt={6}>
+          <Button as={Link} colorScheme="blue" rel="noopener noreferrer">
             Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
+          </Button>
+          <Button as={Link} rel="noopener noreferrer" variant="outline">
             Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          </Button>
+        </Flex>
+      </Flex>
+      <Flex as="footer" mt={10} justify="space-around">
+        <Link
+          href="https://nextjs.org/learn"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
           Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        </Link>
+        <Link
+          href="https://vercel.com/templates?framework=next.js"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
           Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        </Link>
+        <Link
+          href="https://nextjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
           Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </Link>
+      </Flex>
+    </Box>
   );
 }
